801a838772e8438ddfd7a66030d5468ab5c078e0
Brittney Vaughn <34939860+BrittneyRaquel02@users.noreply.github.com>
2018-03-05 18:09:55 -0500

    Update ang.js

--- ang.js
+++ ang.js
@@ -1,20 +1,13 @@
-
-(function (){
-angular
-.module('app',[])
-.controller("ModelController",ModelController)
- function ModelController(){
-  var vm = this;
-  vm.listOfItems = [];
-  vm.addItem = function(listItem) {
-    vm.listOfItems.push(listItem);
-    vm.listItem = "";
-  }
-
-  vm.remove =function(){
-    vm.listOfItems.splice(this.index, 1);
-  }
-
-};
-
+(function(){
+  angular.module('app', ['ngRoute'])
+  .config(function($routeProvider){
+   $routeProvider
+    .when('/', {
+      template: '<welcome></welcome>'
+    })
+    .when("/app", {
+      template: '<taskfolder>'
+    })
+  
+  });
 })();
