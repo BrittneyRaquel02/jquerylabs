1835ac60997388a293fea326fcc1f1e2eb43f1c7
Brittney Vaughn <34939860+BrittneyRaquel02@users.noreply.github.com>
2018-03-05 18:15:09 -0500

    Create data-factory.js

---
+++ data-factory.js
@@ -0,0 +1,16 @@
+form-container {
+margin: auto;
+padding: 50px;
+align-content: center;
+}
+
+background {
+  color: pink;
+}
+
+a {
+font-size: 30px;
+font-style: bold;
+color: black;
+float: left;
+}
